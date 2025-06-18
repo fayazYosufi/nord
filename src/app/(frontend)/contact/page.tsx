b5785352fb02c6bcdd55146e90391e1fa6edabe9
fayazYosufi <ebtekar.af2020@gmail.com>
2025-06-18 19:01:02 +0430

    fsdfsdf

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -7,16 +7,19 @@ const Contact = () => {
   const { apiURL } = useMyContext()
   return (
     <div className="page">
-      <img src="/media/homeNew.PNG" alt="img" />
-      <img src="./media/homeNew.PNG" alt="img" />
-      <img src="../media/homeNew.PNG" alt="img" />
-      <img src="../../media/homeNew.PNG" alt="img" />
-      <img src="../../../media/homeNew.PNG" alt="img" />
-      <img src="../../../../media/homeNew.PNG" alt="img" />
-      <img src="../../../../../media/homeNew.PNG" alt="img" />
+      <img src="/media/contact.PNG" alt="img" />
+      <img src="./media/contact.PNG" alt="img" />
+      <img src="../media/contact.PNG" alt="img" />
+      <img src="../../media/contact.PNG" alt="img" />
+      <img src="../../../media/contact.PNG" alt="img" />
+      <img src="../../../../media/contact.PNG" alt="img" />
+      <img src="../../../../../media/contact.PNG" alt="img" />
 
-      <img src={`${apiURL.replace('/api', '')}/media/homeNew.PNG`} alt="img" />
-      <img src={`${apiURL.replace('/api', '')}/media/file/homeNew.PNG`} alt="img" />
+      <img src={`${apiURL.replace('/api', '')}/media/contact.PNG`} alt="img" />
+      <img src={`${apiURL.replace('/api', '')}/media/file/contact.PNG`} alt="img" />
+
+      <img src={`${apiURL}/media/file/contact.PNG`} alt="img" />
+      <img src={`${apiURL}/media/contact.PNG`} alt="img" />
 
       <h1>Contacts</h1>
     </div>
