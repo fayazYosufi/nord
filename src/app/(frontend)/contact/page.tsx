f986ae4f36abe7876744b467dfa53ddef84e85c2
fayazYosufi <ebtekar.af2020@gmail.com>
2025-06-18 18:39:28 +0430

    fsdf

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="page">
-      <img src="../../../media/homeNew.PNG" alt="" />
+      <img src="/media/homeNew.PNG" alt="" />
       <h1>Contacts</h1>
     </div>
   )
