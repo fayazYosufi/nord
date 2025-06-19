fe883fb8404286534e04bcce862c00452f2f9bf7
fayazYosufi <ebtekar.af2020@gmail.com>
2025-06-19 10:07:29 +0430

    dsf

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,25 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['yourdomain.com'], // Replace with your actual domain
-  },
-  // Other Next.js config options here
-}
-
-module.exports = {
-  images: {
     domains: [
       'localhost',
       'localhost:3000',
       'https://nord-bay.vercel.app/',
-      'your-bucket.s3.amazonaws.com', // if using S3
-    ],
+      'your-bucket.s3.amazonaws.com',
+    ], // if using S3], // Replace with your actual domain
   },
+  // Other Next.js config options here
 }
+
+// module.exports = {
+//   images: {
+//     domains: [
+//       'localhost',
+//       'localhost:3000',
+//       'https://nord-bay.vercel.app/',
+//       'your-bucket.s3.amazonaws.com', // if using S3
+//     ],
+//   },
+// }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
