c648fb366e37917c85f773ee1463e9017f35a1df
fayazYosufi <ebtekar.af2020@gmail.com>
2025-06-21 11:05:00 +0430

    sdfdsf

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -2,42 +2,25 @@
 
 import React from 'react'
 import { useMyContext } from '../theContext'
-import path from 'path'
+// import path from 'path'
 
 const Contact = () => {
   const { sections } = useMyContext()
   console.log(sections[0]?.ImgUrl?.url)
-  const apiURL = 'https://localhost:3000'
+  // const apiURL = 'https://localhost:3000'
   // const apiURL = 'https://nord-bay.vercel.app/api'
 
   return (
     <div className="page">
-      {/* // <img src="./media/contact.PNG" alt="img" /> */}
-      {/* <img src="/media/contact.PNG" alt="img" />
-      <img src="../media/contact.PNG" alt="img" />
-      <img src="../../media/contact.PNG" alt="img" />
-      <img src="../../../media/contact.PNG" alt="img" />
-      <img src="../../../../media/contact.PNG" alt="img" />
-      <img src="../../../../../media/contact.PNG" alt="img" />
-
-      <img src={`${apiURL.replace('/api', '')}/media/contact.PNG`} alt="img" />
-      <img src={`${apiURL.replace('/api', '')}/media/file/contact.PNG`} alt="img" />
-
-      <img src={`${apiURL}/media/file/contact.PNG`} alt="img" />
-      <img src={`${apiURL}/media/contact.PNG`} alt="img" />
-
-      <img src={path.join(__dirname, '../../media/contact.PNG')} alt="img" />
-      <img src={path.join(__dirname, '../media/contact.PNG')} alt="img" />
-      <img src={path.join(__dirname, './media/contact.PNG')} alt="img" />
-      <img src={path.join(__dirname, '/media/contact.PNG')} alt="img" /> */}
-
-      {sections?.map((imgUrl, i) => <img key={i} src={imgUrl?.ImgUrl?.url} />)}
+      {sections?.map((imgUrl, i) => <img key={i} src={imgUrl?.ImgUrl?.url} alt="000" />)}
 
       {sections?.map((imgUrl, i) => (
-        <img key={i} src={`/${imgUrl?.ImgUrl?.url.replace('/api/media/file/', '')}`} />
+        <img key={i} src={`/${imgUrl?.ImgUrl?.url.replace('/api/media/file/', '')}`} alt="111" />
       ))}
 
-      {sections?.map((imgUrl, i) => <img key={i} src={`/${imgUrl?.ImgUrl?.url.split('/')[3]}`} />)}
+      {sections?.map((imgUrl, i) => (
+        <img key={i} src={`/${imgUrl?.ImgUrl?.url.split('/')[3]}`} alt="222" />
+      ))}
 
       <h1>Contacts</h1>
     </div>
